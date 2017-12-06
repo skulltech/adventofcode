1bf7fd9bfad004a3a11640c67c3b385d5a5836ef
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-06 05:52:14 +0530

    Solve part two of day five

---
+++ day5/part2.py
@@ -0,0 +1,26 @@
+def solve(inp):
+	inp = [int(elem) for elem in inp.split()]
+	count = 0
+	index = 0
+
+	while index in range(len(inp)):
+		val = inp[index]
+		if val > 2:
+			inp[index] = inp[index] - 1
+		else:
+			inp[index] = inp[index] +1
+
+		index = val + index
+		count = count + 1
+	return count
+
+
+def main():
+	with open('input.txt', 'r') as f:
+		inp = f.read()
+		print('[*] Reading input from input.txt...')
+	print('[*] The solution is: {}'.format(solve(inp)))
+
+
+if __name__=='__main__':
+	main()
